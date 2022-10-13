fa5c4ca373aaac395fd1f22ce81e85a69002957a
Bence Honvéd <bence.honved@stud.uni-corvinus.hu>
2022-10-13 14:43:57 +0200

    desing commit

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -47,7 +47,7 @@ namespace UserMaintenance
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(238, 13);
+            this.label1.Location = new System.Drawing.Point(233, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 1;
