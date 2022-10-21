b31bbd83a16d4ef76cec1e047d33295a6601a264
honvedbence <bence.honved@stud.uni-corvinus.hu>
2022-10-21 12:13:09 +0200

    feladat3

--- 5Het_CR4RMU/5Het_CR4RMU/Form1.cs
+++ 5Het_CR4RMU/5Het_CR4RMU/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5Het_CR4RMU.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,30 @@ namespace _5Het_CR4RMU
         public Form1()
         {
             InitializeComponent();
+            GetExchange();
+        }
+
+
+        private void GetExchange()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
