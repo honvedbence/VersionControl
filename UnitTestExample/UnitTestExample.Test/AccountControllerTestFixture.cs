b05faac9507bdef6e0e1c8d600a1af906f783df8
Bence Honvéd <bence.honved@stud.uni-corvinus.hu>
2022-11-24 18:05:48 +0100

    Register függvény HappyPath

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -29,16 +30,42 @@ namespace UnitTestExample.Test
 
         }
 
-        [   Test,
-            TestCase("abcdABCD",false),
-            TestCase("ABCD1234", false),
-            TestCase("abcd1234", false),
-            TestCase("Ab1234", false),
-            TestCase("Abcd1234", true)]
+        /*[   Test,
+            TestCase("abcdABCD"),
+            TestCase("ABCD1234"),
+            TestCase("abcd1234"),
+            TestCase("Ab123"),
+            TestCase("Abcd1234")]
 
-        public void TestValidatePassword()
+        public bool TestValidatePassword(string password)
         {
+             
+            Regex regex = new Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{7,}$");
 
+            if (regex.IsMatch(password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } */
+
+        [   Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567")]
+
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.Register(email, password);
+
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+            
         }
     }
 }
