86b0e4cbd9839aca064ea8153d98f39258997552
honvedbence <bence.honved@stud.uni-corvinus.hu>
2022-12-02 12:06:29 +0100

    Osztályok létrehozása

---
+++ Week09/Week09/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfBirths { get; set; }
+        public double P { get; set; }
+    }
+}
