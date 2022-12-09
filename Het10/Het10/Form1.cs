58519aa25fe69f8582c83d465943ef89eb9df359
honvedbence <bence.honved@stud.uni-corvinus.hu>
2022-12-09 12:04:17 +0100

    Induló populáció felépítése

--- Het10/Het10/Form1.cs
+++ Het10/Het10/Form1.cs
@@ -17,6 +17,11 @@ namespace Het10
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +31,13 @@ namespace Het10
 
             gc.AddPlayer();
             gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
